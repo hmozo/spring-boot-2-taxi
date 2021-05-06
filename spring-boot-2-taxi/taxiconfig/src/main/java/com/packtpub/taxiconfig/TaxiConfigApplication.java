3fc172fd1b9ce02a730b0137cbd8f5fe70ed5b14
hmozo <hmozo@outlook.com>
2021-05-06 20:29:07 +0200

    Taxi Redis config

---
+++ spring-boot-2-taxi/taxiconfig/src/main/java/com/packtpub/taxiconfig/TaxiConfigApplication.java
@@ -0,0 +1,14 @@
+package com.packtpub.taxiconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiConfigApplication.class, args);
+
+	}
+
+}
