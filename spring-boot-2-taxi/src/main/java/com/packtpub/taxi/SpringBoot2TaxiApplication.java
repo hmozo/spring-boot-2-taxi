2b1abf738e3c20a474886f159b701d259cd5d389
hmozo <hmozo@outlook.com>
2021-05-06 21:00:38 +0200

    taxi root app

---
+++ spring-boot-2-taxi/src/main/java/com/packtpub/taxi/SpringBoot2TaxiApplication.java
@@ -0,0 +1,13 @@
+package com.packtpub.taxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2TaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2TaxiApplication.class, args);
+	}
+
+}
---
+++ spring-boot-2-taxi/src/test/java/com/packtpub/taxi/SpringBoot2TaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packtpub.taxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2TaxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
