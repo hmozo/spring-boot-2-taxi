68510b02178a284618ed7e4434d667666c13c47e
hmozo <hmozo@outlook.com>
2021-05-12 08:23:03 +0200

    Taxi config

--- spring-boot-2-taxi/taxiconfig/src/main/java/com/packtpub/taxiconfig/config/RedisConfig.java
+++ spring-boot-2-taxi/taxiconfig/src/main/java/com/packtpub/taxiconfig/config/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 @Configuration
@@ -13,11 +14,12 @@ public class RedisConfig {
 	
 	@Bean
 	public LettuceConnectionFactory redisConnectionfactory() {
-		return new LettuceConnectionFactory("db", 6379);
+		return new LettuceConnectionFactory("localhost", 6379);
 	}
 	
 	@Bean
 	public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(ReactiveRedisConnectionFactory connectionFactory){
 		return new ReactiveRedisTemplate<>(connectionFactory, RedisSerializationContext.string());
 	}
+
 }
